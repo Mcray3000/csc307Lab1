878b2de28786b35521aeb44a230a00ff71fbc434
Mcray3000 <69219980+Mcray3000@users.noreply.github.com>
2024-10-03 10:22:06 -0700

    step 5 done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -21,6 +21,16 @@ app.get('/users', (req, res) => {
   }
 });
 
+app.get('/users/:id', (req, res) => {
+  const id = req.params['id'];  // or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send('Resource not found.');
+  } else {
+    res.send(result);
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
@@ -29,6 +39,9 @@ app.listen(port, () => {
 const findUserByName = (name) => {
   return users['users_list'].filter((user) => user['name'] === name);
 };
+
+const findUserById = (id) =>
+    users['users_list'].find((user) => user['id'] === id);
 
 const users = {
   users_list: [
